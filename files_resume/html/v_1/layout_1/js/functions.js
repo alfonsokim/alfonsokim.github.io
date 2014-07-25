c91a86b0d92235afdf0769a83d411a594b899849
Alfonso Kim <alfonsokim84@gmail.com>
2014-07-24 20:41:18 -0500

    template del sitio

---
+++ files_resume/html/v_1/layout_1/js/functions.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+			
+			$("a.contact").fancybox({
+'padding': 10, // optional
+'width': 650, //or whatever you like
+'height': 350,
+'type': 'iframe' ,
+'scrolling': 'no'
+});		
+$("a.box").fancybox({
+				'titleShow'		: true,
+				'transitionIn'	: 'elastic',
+				'transitionOut'	: 'elastic',
+				'titlePosition'	: 'over',
+				 'overlayOpacity': '0.8' 
+			});
+			
+			$("a[rel=group]").fancybox({
+				'transitionIn'		: 'none',
+				'transitionOut'		: 'none',
+				'titlePosition' 	: 'over',
+				'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+					return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+				}
+			});
+		});
+			
